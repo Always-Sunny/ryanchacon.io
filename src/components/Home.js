1830b2e1c109c635c2a1158affe527defa97ed76
Ryan <ryan.chacon103@yahoo.com>
2017-10-05 16:19:13 -0700

    added tagline

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom';
 import Typed from 'typed.js';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onComplete: false,
+    };
+  }
   componentDidMount() {
-    // You can pass other options here, such as typing speed, back speed, etc.
     const options = {
       strings: ["ryanchacon.io","Ryan Andrew Chacon","RAC"],
+      // strings: ["rac"],
       typeSpeed: 50,
-      backSpeed: 30,
+      backSpeed: 40,
       smartBackspace: true,
-      showCursor: false
-      // onComplete: (self) => {}
+      showCursor: false,
+      onComplete: (self) => {console.log(this.state),this.setState({onComplete: true}), console.log(this.state)}
     };
     // this.el refers to the <span> in the render() method
     this.typed = new Typed(this.el, options);
@@ -23,7 +29,14 @@ class Home extends React.Component {
     this.typed.destroy();
   }
 
+
   render() {
+    const isCompleted = this.state.onComplete;
+    let fadeIn = null;
+    if (isCompleted) {
+       fadeIn = <div className="tagline">Built With NodeJS and React</div>
+    }
+
     return (
       <div className="Home">
         <div className="heading">
@@ -32,6 +45,7 @@ class Home extends React.Component {
             style={{ whiteSpace: 'pre' }}
             ref={(el) => { this.el = el; }}
             />
+          {fadeIn}
         </div>
       </div>
     );
