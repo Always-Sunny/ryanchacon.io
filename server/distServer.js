25490c6ad4f9192a78c46065a8c45f8011467f7c
Ryan <ryan.chacon103@yahoo.com>
2018-01-28 21:44:57 -0800

    testing new procfile postbuild

--- server/distServer.js
+++ server/distServer.js
@@ -1,18 +1,41 @@
+/*eslint-disable no-console */
 import express from 'express';
 import path from 'path';
+import { StaticRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from '../client/src/components/App';
+import routes from '../client/src/routes';
 
-/*eslint-disable no-console */
+
 
 const port = 3000;
 const app = express();
+const context = {};
 
-app.use(express.static('dist'));
+
+// app.use(express.static('dist'));
+app.use(express.static(`${__dirname}/../dist`));
+
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
+// app.get('*', (req, res) => {
+//   res.render('index', {
+//     content: ReactDOMServer.renderToString(
+//       // might have to wrap this in a <Provider component later from redux
+//       <StaticRouter location={req.url} context={context}>
+//         <App routes={routes} />
+//       </StaticRouter>),
+//   });
+// });
+
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
-app.use(express.static(`${__dirname}/../dist`));
 
 app.listen(port, () => {
   console.info('Express listening on port', port);
 });
+//
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,56 +1,147 @@
 const path = require('path');
-import webpack from 'webpack';
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
-const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production')
-};
+const staticSourcePath = path.join(__dirname, 'public');
+const srcPath = path.join(__dirname, 'client', 'src');
+const buildPath = path.join(__dirname, 'dist');
+const viewPath = path.join(__dirname, 'client', 'views');
 
 module.exports = {
-  entry: './client/src/index.js',
-  output: {
-    path: `${__dirname}/dist`,
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  target: 'web',
-  devtool: 'source-map',
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.DefinePlugin(GLOBALS),
-    new webpack.optimize.UglifyJsPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-          },
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-        })),
-      },
-      {
-        test: /\.(jpe?g|png|gif|ico)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              // name: '[name].[ext]'
+    devtool: 'cheap-module-source-map',
+    entry: {
+        app: path.resolve(srcPath, 'index.js')
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+        modules: [
+            srcPath,
+            path.resolve(__dirname, 'node_modules')
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: 'vendor.[chunkhash].js',
+            minChunks (module) {
+                return module.context && module.context.indexOf('node_modules') >= 0;
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                screw_ie8: true,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                if_return: true,
+                join_vars: true
             },
-          },
-        ],
-      },
+            output: {
+                comments: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [
+                    autoprefixer({
+                        browsers: [
+                            'last 3 version',
+                            'ie >= 10'
+                        ]
+                    })
+                ],
+                context: staticSourcePath
+            }
+        }),
+        new webpack.HashedModuleIdsPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(viewPath, 'index.ejs'),
+            path: buildPath,
+            excludeChunks: ['base'],
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: true,
+                collapseInlineTagWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true
+            }
+        }),
+        new PreloadWebpackPlugin({
+            rel: 'preload',
+            as: 'script',
+            include: 'all',
+            fileBlacklist: [/\.(css|map)$/, /base?.+/]
+        }),
+        new ScriptExtHtmlWebpackPlugin({
+            defaultAttribute: 'defer'
+        }),
+        new ExtractTextPlugin({
+            filename: '[name].[contenthash].css',
+            allChunks: true
+        }),
+        new StyleExtHtmlWebpackPlugin({
+            minify: true
+        }),
+        new CompressionPlugin({
+            asset: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ],
+                include: srcPath
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { minimize: true } },
+                        'postcss-loader',
+                        'sass-loader'
+                    ]
+                })
+            },
+            {
+                test: /\.(eot?.+|svg?.+|ttf?.+|otf?.+|woff?.+|woff2?.+)$/,
+                use: 'file-loader?name=assets/[name]-[hash].[ext]'
+            },
+            {
+                test: /\.(png|gif|jpg|svg)$/,
+                use: [
+                    'url-loader?limit=20480&name=assets/[name]-[hash].[ext]'
+                ],
+                include: staticSourcePath
+            }
+        ]
+    }
 };
